abbc91882cc755d06ae80f1f833ecc902b98eb76
Arom <arom.sanchez@tecsup.edu.pe>
2024-09-12 00:37:47 -0500

    "Se agregaron estilos CSS para encabezado, diseño y animación de precarga, estructura HTML actualizada y JavaScript agregado para animación de precarga"

---
+++ js/btn_menu.js
@@ -0,0 +1,5 @@
+const div = document.querySelector('.center')
+
+div.addEventListener('click', () => {
+    div.classList.toggle('animated')
+})
---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    setTimeout(function () {
+      document.body.classList.add('loaded'); 
+    }, 2000); 
+});
