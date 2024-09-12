1ad419ed10c87dd3ee84bffe34bfa48a8c45e5c0
Arom <arom.sanchez@tecsup.edu.pe>
2024-09-12 10:44:00 -0500

    "Se agregó un menú desplegable al encabezado con estilo CSS y funcionalidad de alternancia de JavaScript"

--- js/btn_menu.js
+++ js/btn_menu.js
@@ -1,5 +1,12 @@
 const div = document.querySelector('.center')
-
+const menu = document.querySelector('.opciones-desplegado')
 div.addEventListener('click', () => {
     div.classList.toggle('animated')
+    if (menu.style.opacity === '1') {
+        menu.style.opacity = '0';
+        menu.style.transform = 'translateX(-150px)';
+      } else {
+        menu.style.opacity = '1';
+        menu.style.transform = 'translateX(0)';
+      }
 })
