e1f604c797c5f2ab38a3dac6416a8f78eb92f8f1
ZaitDioses <zaitdioses@gmail.com>
2024-10-12 18:52:56 -0500

    Delete Vida-submarina directory

--- Vida-submarina/js/openDialog.js
+++
@@ -1,6 +0,0 @@
-const openButton = document.querySelector('.btn_play');
-const closeButton = document.querySelector('#closeDialog');
-const dialog = document.getElementById('videoDialog');
-
-openButton.addEventListener('click', () => dialog.showModal());
-closeButton.addEventListener('click', () => dialog.close());
